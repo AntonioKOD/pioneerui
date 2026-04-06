68ba74d640fa357676426da8955721a0925822d9
Antonio Kodheli <antonio_kodheli@icloud.com>
2026-04-06 19:08:12 -0400

    remove fake community spotlight section

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -2,7 +2,6 @@ import { Hero3D }              from "@/components/sections/hero-3d"
 import { StatsBar }            from "@/components/sections/stats-bar"
 import { ComponentShowcase }   from "@/components/sections/component-showcase"
 import { HowItWorks }          from "@/components/sections/how-it-works"
-import { CommunitySpotlight }  from "@/components/sections/community-spotlight"
 import { CTASection }          from "@/components/sections/cta-section"
 
 export default function Home() {
@@ -12,7 +11,6 @@ export default function Home() {
       <StatsBar />
       <ComponentShowcase />
       <HowItWorks />
-      <CommunitySpotlight />
       <CTASection />
     </>
   )
