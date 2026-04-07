43fe2aeaca1d9d4d23def4e206372844a440cb03
Antonio Kodheli <antonio_kodheli@icloud.com>
2026-04-06 22:31:33 -0400

    remove fake stats bar from homepage

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,5 +1,4 @@
 import { Hero3D }              from "@/components/sections/hero-3d"
-import { StatsBar }            from "@/components/sections/stats-bar"
 import { ComponentShowcase }   from "@/components/sections/component-showcase"
 import { HowItWorks }          from "@/components/sections/how-it-works"
 import { CTASection }          from "@/components/sections/cta-section"
@@ -8,7 +7,6 @@ export default function Home() {
   return (
     <>
       <Hero3D />
-      <StatsBar />
       <ComponentShowcase />
       <HowItWorks />
       <CTASection />
